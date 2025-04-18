d4eceb45451f6f84b61151987a5b3a6b6d9b6c83
JuliaAraujoOliveira <julia.oliveira@sptech.school>
2025-04-18 18:49:09 -0300

    adicionando crud de usuario finalizado ao novo repositorio e arrumando versionamento de codigo

--- preciososLacos/src/main/kotlin/com/lacos_preciosos/preciososLacos/controller/UsuarioController.kt
+++ preciososLacos/src/main/kotlin/com/lacos_preciosos/preciososLacos/controller/UsuarioController.kt
@@ -38,6 +38,44 @@ class UsuarioController(private val repositorio: UsuarioRepository) {
         }
     }
 
+
+    @GetMapping("/pesquisar")
+    fun listarPorNome(@RequestParam nomeCompleto: String ): ResponseEntity<List<Usuario>> {
+        val usuarioEncontrado = repositorio.findByNomeCompletoContains(nomeCompleto)
+
+        return if(usuarioEncontrado.isEmpty()){
+            ResponseEntity.status(204).build()
+        }else{
+            ResponseEntity.status(200).body(usuarioEncontrado)        }
+    }
+
+    //Atualizar o usuario no banco de dados
+    @PutMapping("/{nomeCompleto}")
+    fun atualizarDados(@PathVariable nomeCompleto: String, @RequestBody usuario: Usuario): ResponseEntity<Usuario> {
+        val usuarioEncontrado = repositorio.findByNomeCompletoContains(nomeCompleto)
+
+        return if (usuarioEncontrado.isNotEmpty()) {
+            usuarioEncontrado.forEach { it.nomeCompleto = usuario.nomeCompleto }
+            repositorio.saveAll(usuarioEncontrado)
+            ResponseEntity.status(200).body(usuario)
+        } else {
+            ResponseEntity.status(404).build()
+        }
+    }
+
+
+    //Deletando usuario no banco de dados
+    @DeleteMapping("/{nomeCompleto}")
+    fun deletarUsuario(@PathVariable nomeCompleto: String): ResponseEntity<Void> {
+        val usuariosEncontrados = repositorio.findByNomeCompletoContains(nomeCompleto)
+        return if (usuariosEncontrados.isNotEmpty()) {
+            usuariosEncontrados.forEach { repositorio.delete(it) }
+            ResponseEntity.status(204).build()
+        } else {
+            ResponseEntity.status(404).build()
+        }
+    }
+
     @PatchMapping("/login")
     fun login(@RequestBody autenticacao: AutenticacaoUsuarioDTO): ResponseEntity<Void> {
         val response = repositorio.autenticarUsuarioTRUE(autenticacao.email, autenticacao.senha)
