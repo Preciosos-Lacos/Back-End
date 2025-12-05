b89351ac692f70c9f572f463c21c7b8a38c4940a
EduardoMiyasakii <eduardo.miyasaki@sptech.school>
2025-12-04 21:17:22 -0300

    Adicionando associacao de tipos de laco ao modelo

--- preciososLacos/src/main/kotlin/com/lacos_preciosos/preciososLacos/dto/tipoLaco/CadastroTipoLacoDTO.kt
+++ preciososLacos/src/main/kotlin/com/lacos_preciosos/preciososLacos/dto/tipoLaco/CadastroTipoLacoDTO.kt
@@ -2,4 +2,5 @@ package com.lacos_preciosos.preciososLacos.dto.tipoLaco
 
 data class CadastroTipoLacoDTO(val nome: String,
                                val preco: Double,
-                               val imagemBase64: String?)
+                               val imagemBase64: String?,
+                               val modelosIds: List<Int>? )
